1f9a39d3899a648c9a91633accfca7f3ea2e5173
Declend Mcmurry <declend.mcmurry@gmail.com>
2023-11-09 15:15:01 -0600

    Timer Works

---
+++ Assets/Scripts/DeclendScripts/Countdown.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using SuperPupSystems.Helper;
+
+public class Countdown : MonoBehaviour
+{
+    public Timer timer;
+    public float timeLeft;
+    public TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer.StartTimer();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft = (int)timer.timeLeft;
+        Debug.Log(timeLeft);
+        text.text = timeLeft.ToString();
+    }
+}
