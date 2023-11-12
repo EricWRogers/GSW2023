0ec29665b36682ded0567819a435cb8ec6dc816c
ldroberts02 <ldroberts02@gmail.com>
2023-11-11 19:18:33 -0600

    Start of New Character Select!
    Moved globalvariables script over to gamemanager script, created and fleshed out input and event systems for the new character select, and changed the build and other settings such that other scenes now point to the new scene,
    NOT DONE YET: Colorways for each character, p2 stuff (waiting until p1 is done)

--- Assets/Scripts/CharacterSelect/New/ChEventScript.cs
+++ Assets/Scripts/CharacterSelect/New/ChEventScript.cs
@@ -5,42 +5,67 @@ using UnityEngine.UI;
 
 public class ChEventScript : MonoBehaviour
 {
-    public Sprite player1Selection;
-    public Sprite player2Selection;
-    public string player1SelName;
-    public string player2SelName;
+    public GameObject player1Selection;
+    public GameObject player2Selection;
+    public int player1SelectionInt;
+    public int player2SelectionInt;
+    public Character characterObject;
+    public List<Character> charList;
+    int charListNum;
     void Start()
     {
-        player1Selection = GameObject.Find("Player 1 Selection").GetComponent<Image>().sprite;
-        player1SelName = "Kable";
-        player2Selection = GameObject.Find("Player 2 Selection").GetComponent<Image>().sprite;
-        player1SelName = "Other Guy";
+        player1Selection = GameObject.Find("Player 1 Selection");
+        player2Selection = GameObject.Find("Player 2 Selection");
+        charListNum = charList.Count;
     }
     void Update()
     {
-        
+        player2Selection.GetComponent<Image>().sprite = charList[player2SelectionInt].characterSprite; // put this in p2input event area
+
     }
     public void P1Input(string input)
     {
         switch(input)
         {
             case "Left":
-                print("Left");
-
+                //move down the index by 1, if at minimum, set it to maximum
+                if (player1SelectionInt <= 0)
+                {
+                    player1SelectionInt = (charListNum - 1);
+                }
+                else if (player1SelectionInt <= (charListNum -1))
+                {
+                    player1SelectionInt = player1SelectionInt - 1;
+                }
                 break;
             case "Right":
-                print("Right");
+                //move up index by 1, if at max, set to min
+                if (player1SelectionInt >= (charListNum - 1))
+                {
+                    player1SelectionInt = (charListNum - charListNum);
+                }
+                else if (player1SelectionInt <= (charListNum - 1))
+                {
+                    player1SelectionInt ++;
+                }
+
                 break;
             case "Up":
                 print("Up");
+                
+
                 break;
             case "Down":
                 print("Down");
+                
+
                 break;
         }
+        player1Selection.GetComponent<Image>().sprite = charList[player1SelectionInt].characterSprite;
+        GameManager.Instance.Player1Selection = charList[player1SelectionInt];
     }
-    public void ImageSet(Sprite target,Sprite newTarget)
+    public void P2Input(string input)
     {
-
+        //wait until p1 is done to put anything here, should just copy+paste but with replaced vars
     }
 }
