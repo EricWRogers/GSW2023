16d4d90f93138fafabed1b7cf62390423d23179e
Aiden Thacker <aidenthacker86@gmail.com>
2023-12-07 14:45:00 -0600

    Player Text stays on Screen

--- Assets/Scripts/FollowText.cs
+++ Assets/Scripts/FollowText.cs
@@ -6,11 +6,19 @@ public class FollowText : MonoBehaviour
 {
     public Transform player;
 
+    [SerializeField]
+    private float roof = 5.0f;
+
     public float yOffset = 1.0f;
 
     void Update()
     {
         // Update the position of the empty GameObject to follow the fighter
-        transform.position = player.position + new Vector3(0, yOffset, 0); // Adjust yOffset as needed
+        transform.position = player.position + new Vector3(0, yOffset, 0);
+
+        if (transform.position.y > roof)
+        {
+            transform.position = new Vector3(transform.position.x, roof - 1, 0f);
+        }
     }
 }
